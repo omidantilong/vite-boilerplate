f55dd939b743efd14f6ec711d4c788a6b5eb35f6
Omid Kashan <omid@antilong.com>
2021-11-15 11:14:30 +0000

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react"
-import logo from "./logo.svg"
-import { Header } from "./components/Layout/Header"
 import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-  var piss = "hello"
-  return (
-    <div className="page">
-      <Header />
-    </div>
-  )
+  return <div className="page">Hello</div>
 }
 
 export default App
