575cdddc1e3923f9e19fb97469992e37ab6e7330
Omid Kashan <omid@antilong.com>
2021-11-15 11:15:54 +0000

    update default component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css"
-
+import { Header } from "./components/Header"
 function App() {
-  return <div className="page">Hello</div>
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  )
 }
 
 export default App
