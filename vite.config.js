3f1ae68b80237113c9e1d8743cde353cb333efe8
Omid Kashan <omid@antilong.com>
2021-11-16 23:47:49 +0000

    add absolute resolver to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import eslintPlugin from "@nabla/vite-plugin-eslint"
+import svgrPlugin from "vite-plugin-svgr"
+const path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin({ formatter: "stylish" })],
+  plugins: [react(), svgrPlugin(), eslintPlugin({ formatter: "stylish" })],
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
 })
